71a08c5766262084caa27178177645930662489f
drozd <drozd.dmitriy.etc@gmail.com>
2019-12-11 19:29:31 +0200

    Add comment model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_10_234130) do
+ActiveRecord::Schema.define(version: 2019_12_11_164707) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.text "text"
+    t.string "file"
+    t.bigint "task_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["task_id"], name: "index_comments_on_task_id"
+  end
 
   create_table "projects", force: :cascade do |t|
     t.string "name"
@@ -41,6 +50,7 @@ ActiveRecord::Schema.define(version: 2019_12_10_234130) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "tasks"
   add_foreign_key "projects", "users"
   add_foreign_key "tasks", "projects"
 end
