96baba26ce7ef3824a3292263f8926868dc6caa7
drozd <drozd.dmitriy.etc@gmail.com>
2020-01-23 16:24:52 +0200

    Add alias to ability

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,11 +2,12 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
+    alias_action :create, :read, :update, :destroy, to: :crud
     user ||= User.new
     return unless user.is_a?(User) && user.persisted?
 
-    can %i[create read update destroy], Project, user_id: user.id
-    can %i[create read update destroy], Task, project: { user_id: user.id }
-    can %i[create read update destroy], Comment, task: { project: { user_id: user.id } }
+    can :crud, Project, user_id: user.id
+    can :crud, Task, project: { user_id: user.id }
+    can :crud, Comment, task: { project: { user_id: user.id } }
   end
 end
