1e539eb45f8b187da52680350435ec8d9bf61486
drozd <drozd.dmitriy.etc@gmail.com>
2020-02-17 09:48:05 +0200

    Fixed after review

--- spec/requests/v1/comments_spec.rb
+++ spec/requests/v1/comments_spec.rb
@@ -4,15 +4,15 @@ RSpec.describe 'V1::Comments API', type: :request do
   let(:user) { create(:user) }
   let(:project) { create(:project, user: user) }
   let(:task) { create(:task, project: project) }
-  let!(:comments) { create_list(:comment, 3, task: task) }
-  let(:comment) { create(:comment, task: task) }
-  let(:comment_params) { attributes_for(:comment) }
   let(:headers) { { authorization: JsonWebToken.encode(user_id: user.id), accept: 'application/json' } }
 
   describe 'GET /api/v1/tasks/:task_id/comments' do
     include Docs::V1::Comments::Index
 
     context 'with valid task_id', :dox do
+      let(:comment) { create(:comment, task: task) }
+      let!(:comments) { create_list(:comment, 3, task: task) }
+
       before { get api_v1_task_comments_path(task), headers: headers }
 
       it { expect(response.parsed_body.size).to eq(3) }
@@ -46,6 +46,7 @@ RSpec.describe 'V1::Comments API', type: :request do
 
   describe 'POST /api/v1/tasks/:task_id/comments' do
     include Docs::V1::Comments::Create
+    let(:comment_params) { attributes_for(:comment) }
 
     context 'with valid params', :dox do
       before { post api_v1_task_comments_path(task), headers: headers, params: comment_params, as: :json }
@@ -84,9 +85,12 @@ RSpec.describe 'V1::Comments API', type: :request do
 
   describe 'DELETE /api/v1/tasks/:task_id/comments/:id' do
     include Docs::V1::Comments::Delete
+    let(:comment) { create(:comment, task: task) }
 
     context 'with valid id', :dox do
-      before { delete api_v1_comment_path(id: comments.first.id), headers: headers }
+      let(:comment) { create(:comment, task: task) }
+
+      before { delete api_v1_comment_path(comment), headers: headers }
 
       it { expect(response).to have_http_status(:no_content) }
     end
@@ -100,7 +104,7 @@ RSpec.describe 'V1::Comments API', type: :request do
     context 'when user unauthorized', :dox do
       let(:headers) { { authorization: nil, accept: 'application/json' } }
 
-      before { delete api_v1_comment_path(id: comments.first.id), headers: headers }
+      before { delete api_v1_comment_path(comment), headers: headers }
 
       it { expect(response).to have_http_status(:unauthorized) }
     end
@@ -109,7 +113,7 @@ RSpec.describe 'V1::Comments API', type: :request do
       let(:user_forbidden) { create(:user, username: 'test4') }
       let(:headers) { { authorization: JsonWebToken.encode(user_id: user_forbidden.id), accept: 'application/json' } }
 
-      before { delete api_v1_comment_path(id: comments.first.id), headers: headers }
+      before { delete api_v1_comment_path(comment), headers: headers }
 
       it { expect(response).to have_http_status(:forbidden) }
     end
